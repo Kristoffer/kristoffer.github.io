b3feca4bea845d485253099bc04cdad5fccfed6e
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2023-12-22 15:22:01 +0100

    asdf

---
+++ cyan-chroma/astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import alpine from '@astrojs/alpinejs';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://kristoffer.github.io',
+  //base: '/cyan-chroma',
+  integrations: [alpine()],
+});
