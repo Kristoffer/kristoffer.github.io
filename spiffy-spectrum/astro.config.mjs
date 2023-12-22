aea6a6e8d8e661d776c1852848228c4035e330d1
Kristoffer Egefelt <kristoffer.egefelt@jppol.dk>
2023-12-22 22:33:07 +0100

    install tailwind

--- spiffy-spectrum/astro.config.mjs
+++ spiffy-spectrum/astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://kristoffer.github.io',
   //base: '/spiffy-spectrum'
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), tailwind()]
 });
